c6d6ab64fcd71dac4e6ae399b06c3dc7423fc656
Varsharani Ambi <68917117+varsha-ambi@users.noreply.github.com>
2020-11-08 20:42:53 +0530

    ass

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <div id="root">
+        <h1 data-ns-test="project-name">
+          Direct selling between Farmer and Trader
+        </h1>
+        <p data-ns-test="project-description">
+          In this project i want to provide easy way fro farmer and trader to
+          buy and cell their products
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default App;
