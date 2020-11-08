422e2d594fc8fe25d6680d26194af45a8f4df525
Varsharani Ambi <68917117+varsha-ambi@users.noreply.github.com>
2020-11-08 20:58:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,10 @@ const App = () => {
     <>
       <div id="root">
         <h1 data-ns-test="project-name">
-          Direct selling between Farmer and Trader
+          Doctor App
         </h1>
         <p data-ns-test="project-description">
-          In this project i want to provide easy way fro farmer and trader to
-          buy and cell their products
+          In this project patient easily get online appointment.
         </p>
       </div>
     </>
